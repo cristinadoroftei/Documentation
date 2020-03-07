fd644f99775109f77ffa82315368fa383ee8cd0d
CristinaDoroftei <dorofteicristina19@gmail.com>
2020-03-07 10:07:57 +0100

    added dropdown, styling the page and made the beginning of first chapter

--- public/index.js
+++ public/index.js
@@ -5,12 +5,23 @@ $(function() {
 $.getJSON("data.json",function(data){
 userObj = data;
 homeClick()
+
+let dropdown = document.getElementsByClassName("dropdown-btn");
+let i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  dropdownContent.classList.toggle('dropdown-container-open')
+  });
+}
 });
 })
 
 function changeClass(){
-    var navBar = document.getElementById('menu')
-    var page = document.getElementById('content')
+    let navBar = document.getElementById('menu')
+    let page = document.getElementById('content')
     navBar.classList.toggle('nav-closed')
     page.classList.toggle('page-closed')
 }
@@ -18,7 +29,9 @@ function changeClass(){
 function homeClick(){
     $(".documentation").remove()
     $(".page").append('<div class="documentation"></div>');
-    $( ".documentation" ).append( `${userObj.lessons[0].title.join('\n')}` );
+    $( ".documentation" ).append( `${userObj.whatIsJavascript.title.join('\n')}` );
+    $(".info-text").css("font-size", "1.3em")
+    $(".info-text").css("margin", "0.3em")
 
 }
 
